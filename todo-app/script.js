ae496b50eab1d5839e536074553f8b1b6f4b84f4
KubaTworek <kubatvorek@gmail.com>
2022-01-04 23:34:04 +0100

    To-Do List version 2.2
    added drag&drop to project

--- todo-app/script.js
+++ todo-app/script.js
@@ -10,6 +10,7 @@ class TasksList {
     numberActive = 0;
     numberFinished = 0;
 
+
     addActive(element) {
         this.activeTasks.push(element)
         this.numberActive++;
@@ -36,11 +37,18 @@ class TasksList {
     render() {
         activeTasksList.innerHTML = ``;
         finishedTasksList.innerHTML = ``;
+        let i = 0;
 
-        for(let task of this.activeTasks){
+        for(let task of this.activeTasks){        
             const taskEl = document.createElement('li');
             taskEl.className = 'active-task__element task-element';
             taskEl.innerHTML = `<p>${task.text}</p>`;
+            taskEl.draggable = true;
+            taskEl.id = Math.random();
+            taskEl.addEventListener('dragstart', event => {
+                event.dataTransfer.setData('text/plain', taskEl.id);
+                event.dataTransfer.effectAllowed = 'move';
+            });
             activeTasksList.appendChild(taskEl);
             taskEl.addEventListener('click', () => {
                 this.addToFinished(task);
@@ -52,11 +60,13 @@ class TasksList {
             const taskEl = document.createElement('li');
             taskEl.className = 'finished-task__element task-element';
             taskEl.innerHTML = `<p>${task.text}</p>`;
-            finishedTasksList.appendChild(taskEl);
-            taskEl.addEventListener('click', () => {
-                this.removeElement(task);
-                this.render();
+            taskEl.draggable = true;
+            taskEl.id = Math.random();
+            taskEl.addEventListener('dragstart', event => {
+                event.dataTransfer.setData('text/plain', taskEl.id);
+                event.dataTransfer.effectAllowed = 'move';
             });
+            finishedTasksList.appendChild(taskEl);
         }
     }
 }
@@ -110,3 +120,40 @@ function toggleEditBox() {
     background.classList.toggle("shadowed");
     todoEditor.classList.toggle("show");
 }
+
+const garbage = document.querySelector('.garbage');
+
+garbage.addEventListener('dragenter', event => {
+    if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+    }
+    garbage.classList.add('droppable');
+});
+
+garbage.addEventListener('dragover', event => {
+    if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+    }
+});
+
+garbage.addEventListener('dragleave', event => {
+    garbage.classList.remove('droppable');
+});
+
+garbage.addEventListener('drop', event => {
+    const prjId = event.dataTransfer.getData('text/plain');
+    if(document.getElementById(prjId).classList.contains('finished-task__element')) {      
+        tasksList.numberFinished--;
+        numberOfFinished.textContent = tasksList.numberFinished;
+        let pos = tasksList.finishedTasks.indexOf(document.getElementById(prjId));
+        tasksList.finishedTasks.splice(pos, 1);
+        document.getElementById(prjId).parentElement.removeChild(document.getElementById(prjId));
+    } else {
+        tasksList.numberActive--;
+        numberOfActive.textContent = tasksList.numberActive;
+        let pos = tasksList.activeTasks.indexOf(document.getElementById(prjId));
+        tasksList.activeTasks.splice(pos, 1);
+        document.getElementById(prjId).parentElement.removeChild(document.getElementById(prjId));
+    }
+    garbage.classList.remove('droppable');
+});
